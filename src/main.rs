f6a15d4c491d73b45703588fd3cfd4db6bbdeff0
Festus Adekunle <olusolaadek@hotmail.com>
2026-03-08 20:13:03 -0600

    Initialize guessing game with basic functionality and dependencies

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input your guess (1-10): ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => {
+                if num < 1 || num > 10 {
+                    println!("Please type a number between 1 and 10!");
+                    continue;
+                }
+                num
+            }
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        // println!("You guessed: {guess}"); // or using the new syntax
+
+        let rand_num = rand::thread_rng().gen_range(1..=10);
+
+        match guess.cmp(&rand_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        println!("The secret number is: {}", rand_num);
+    }
+
+    // let x = 5;
+    // let y = 10;
+    // println!("x = {x} and y + 2 = {}", y + 2);
+}
